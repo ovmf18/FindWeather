4ea0f58c45d629decc769a8b18015d5c7ddb1033
Osmar Valporto Moreno Filho <osmarfilho2013@gmail.com>
2026-07-22 13:41:39 -0300

    feat: configuracao inicial do projeto e pastas

--- App.tsx
+++ App.tsx
@@ -1,10 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts, Overpass_400Regular, Overpass_600SemiBold, Overpass_700Bold } from '@expo-google-fonts/overpass';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Overpass_400Regular,
+    Overpass_600SemiBold,
+    Overpass_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return null; // Aguardando carregamento da fonte
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={{ fontFamily: 'Overpass_400Regular', fontSize: 18 }}>Projeto FindWeather Criado!</Text>
       <StatusBar style="auto" />
     </View>
   );
